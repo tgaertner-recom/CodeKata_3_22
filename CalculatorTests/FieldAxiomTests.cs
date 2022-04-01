64b9874a9c7c1875a80352c2f4ff499f9a22fa5e
Tobias Gaertner <tgaertner@recom.eu>
2022-04-01 14:45:29 +0200

    All mutants killed

--- CalculatorTests/FieldAxiomTests.cs
+++ CalculatorTests/FieldAxiomTests.cs
@@ -8,6 +8,21 @@ namespace CalculatorTests
     [TestFixture]
     public class FieldAxiomTests
     {
+        [TestCase(1,0,1)]
+        [TestCase(1,1,2)]
+        [TestCase(1,-1,0)]
+        [TestCase(-1,0,-1)]
+        [TestCase(-1,1,0)]
+        [TestCase(-1,-1,-2)]
+        [TestCase(0,0,0)]
+        [TestCase(0,1,1)]
+        [TestCase(0,-1,-1)]
+        public void Test_Add(double op1, double op2, double expected)
+        {
+            var result = SimpleCalculator.Add(op1, op2);
+            result.Should().Be(expected);
+        }
+
         [Test]
         public void Add_should_respect_neutral_element_off_addition()
         {
@@ -15,11 +30,44 @@ namespace CalculatorTests
             result.Should().Be(1);
         }
         
+        [TestCase(1,0,1)]
+        [TestCase(1,1,0)]
+        [TestCase(1,-1,2)]
+        [TestCase(-1,0,-1)]
+        [TestCase(-1,1,-2)]
+        [TestCase(-1,-1,0)]
+        [TestCase(0,0,0)]
+        [TestCase(0,1,-1)]
+        [TestCase(0,-1,1)]
+        public void Test_Diff(double op1, double op2, double expected)
+        {
+            var result = SimpleCalculator.Diff(op1, op2);
+            result.Should().Be(expected);
+        }
+
         [Test]
         public void Diff_should_respect_inverse_element_off_addition()
         {
             var result = SimpleCalculator.Diff(1, 1);
             result.Should().Be(0);
+        }
+        
+        [TestCase(2,0,0)]
+        [TestCase(2,1,2)]
+        [TestCase(2,-1,-2)]        
+        [TestCase(1,0,0)]
+        [TestCase(1,1,1)]
+        [TestCase(1,-1,-1)]
+        [TestCase(-1,0,0)]
+        [TestCase(-1,1,-1)]
+        [TestCase(-1,-1,1)]
+        [TestCase(0,0,0)]
+        [TestCase(0,1,0)]
+        [TestCase(0,-1,0)]
+        public void Test_Mult(double op1, double op2, double expected)
+        {
+            var result = SimpleCalculator.Mult(op1, op2);
+            result.Should().Be(expected);
         }
         
         [Test]
